16e032a6e78ef3672415fa24d50c535a6882349e
ben@blueinno <ben@blueinnotechnology.com>
2025-01-18 12:24:28 +0800

    lesson 2 part 2

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ Q1:
 
 # TODO: Write your code here
 
+
+def greet(human):
+    print(f'Hi, {human}!')
+
+
+greet('Alice')
+greet('Bob')
 ##################################################
 '''
 Q2:
@@ -12,6 +19,12 @@ Q2:
 
 # TODO: Write your code here
 
+
+def greet(name1, name2):
+    print(f'Hi, {name1} and {name2}')
+
+
+greet(name2='Bob', name1='Alex')
 ##################################################
 '''
 Q3:
@@ -19,6 +32,12 @@ Q3:
 
 # TODO: Write your code here
 
+
+def find_rect_area(width, height):
+    print(f'The rectangle area is {width*height}')
+
+
+print(find_rect_area(width=12.3, height=45.6))
 ##################################################
 '''
 Q4:
@@ -26,6 +45,40 @@ Q4:
 
 # TODO: Write your code here
 
+
+def cat_goes():
+    print('Cat goes fiddle-i-fee.')
+    print()
+
+
+def hen_goes():
+    print('Hen goes chimmy-chuck, ', end='')
+    cat_goes()
+
+
+def duck_goes():
+    print('Duck goes quack, quack, ', end='')
+    hen_goes()
+
+
+def goose_goes():
+    print('Goose goes hissy, hissy, ', end='')
+    duck_goes()
+
+
+def bought(animal):
+    print(f'Bought me a {animal} and the {animal} pleased me. I fed my {
+          animal} under yonder tree.', end='')
+
+
+bought('cat')
+cat_goes()
+bought('hen')
+hen_goes()
+bought('duck')
+duck_goes()
+bought('goose')
+goose_goes()
 ##################################################
 '''
 Q5:
